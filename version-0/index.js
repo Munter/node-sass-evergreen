4a98ac2b408bba7b548e57b483cd31eccf146128
Peter Müller <munter@fumle.dk>
2015-05-09 22:36:14 +0200

    First v0.x compatibility wrapper tests

--- version-0/index.js
+++ version-0/index.js
@@ -1,4 +1,37 @@
-var sass = require('node-sass');
-sass.version = require('node-sass/package.json').version;
+'use strict';
 
-module.exports = sass;
+var sass = require('node-sass');
+var extend = require('extend');
+
+sass._version = require('node-sass/package.json').version;
+
+module.exports = extend({}, sass, {
+  render: function (options, cb) {
+    var stats = {};
+
+    var successCallback = function(css) {
+      cb(undefined, {
+        css: new Buffer(css, 'utf8'),
+        map: new Buffer('', 'utf8'),
+        stats: stats
+      });
+    };
+
+    var errorCallback = function (err) {
+      cb({
+        message: '',
+        line: '',
+        column: '',
+        status: '',
+        file: ''
+      });
+    };
+
+    sass.render(extend({}, options, {
+      success: successCallback,
+      error: errorCallback,
+      stats: stats,
+      sourceComments: options.sourceComments === true ? 'normal' : 'none'
+    }));
+  }
+});
--- version-0/version-0-test.js
+++ version-0/version-0-test.js
@@ -1,8 +1,53 @@
+'use strict';
+
 var sass = require('./index');
 var expect = require('unexpected');
 
 describe('node-sass 0.x', function () {
   it('should load the correct version', function () {
-    expect(sass.version, 'to match', /^0.\d+.\d+$/);
+    expect(sass._version, 'to match', /^0.\d+.\d+$/);
+  });
+
+  describe('async rendering', function () {
+    it('should render basic data', function (done) {
+      sass.render({
+        data: 'body { background: hotpink; }'
+      }, function (err, result) {
+        expect(err, 'to be undefined');
+        expect(result, 'to exhaustively satisfy', {
+          css: expect.it('when decoded as', 'utf8', 'to be', 'body {\n  background: hotpink; }\n'),
+          map: expect.it('when decoded as', 'utf8', 'to be', ''),
+          stats: {
+            entry: 'data',
+            start: expect.it('to be a number'),
+            includedFiles: [],
+            end: expect.it('to be a number'),
+            duration: expect.it('to be a number'),
+            sourceMap: undefined
+          }
+        });
+
+        done();
+      });
+    });
+
+    it('should render basic data with sourceComments', function (done) {
+      sass.render({
+        data: 'body { background: hotpink; }',
+        sourceComments: true
+      }, function (err, result) {
+        expect(err, 'to be undefined');
+        expect(result, 'to exhaustively satisfy', {
+          css: expect.it('when decoded as', 'utf8', 'to be', '/* line 1, source string */\nbody {\n  background: hotpink; }\n'),
+          map: expect.it('when decoded as', 'utf8', 'to be', ''),
+          stats: expect.it('to satisfy', {
+            includedFiles: [],
+            sourceMap: undefined
+          })
+        });
+
+        done();
+      });
+    });
   });
 });
