d0f6d6c062b822a5f64ace718a517af5258e5356
Peter Müller <munter@fumle.dk>
2015-05-09 13:52:27 +0200

    Test version

--- version-0/version-0-test.js
+++ version-0/version-0-test.js
@@ -1,6 +1,8 @@
+var sass = require('./index');
+var expect = require('unexpected');
 
 describe('node-sass 0.x', function () {
   it('should load the correct version', function () {
-
+    expect(sass.version, 'to match', /^0.\d+.\d+$/);
   });
 });
