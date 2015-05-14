b5459aa75a8d5120c0724fea983c3bacb663b6e1
Peter Müller <munter@fumle.dk>
2015-05-14 21:57:52 +0200

    Added latest node as reference. Compare all implementations with it

---
+++ version-latest/version-latest-test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var sass = require('./index');
+var expect = require('unexpected');
+
+describe('node-sass latest', function () {
+  it('should load the correct version', function () {
+    expect(sass.version, 'to match', /^3.\d+.\d+$/);
+  });
+
+  it('should show the relevant info', function () {
+    expect(sass.info, 'to match', /\t3.\d+.\d+\t/);
+  });
+
+  // require('../test/test-noTypes')(sass);
+  // require('../test/test-noImporter')(sass);
+  // require('../test/test-noIndentedSyntax')(sass);
+
+  require('../test/test-render')(sass);
+  require('../test/test-renderSync')(sass);
+});
