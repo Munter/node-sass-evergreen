02ee534dcfe4de767a2cee63adeb1eb9fafab47e
Peter Müller <munter@fumle.dk>
2015-05-16 23:41:57 +0200

    Simplify sourcemap test

--- test/test-sourceMap.js
+++ test/test-sourceMap.js
@@ -16,18 +16,13 @@ module.exports = function (sass) {
         expect(err, 'to be null');
         expect(result, 'to exhaustively satisfy', {
           css: expect.it('when decoded as', 'utf8', 'to be', '/* line 1, stdin */\nbody {\n  background: hotpink; }\n'),
-          // map: expect.it('when decoded as', 'utf8', 'when parsed as json', 'to be', '{\n\t"version": 3,\n\t"file": "foo.css",\n\t"sources": [\n\t\t"stdin"\n\t],\n\t"sourcesContent": [],\n\t"mappings": ";AAAA,IAAI,CAAC;EAAE,UAAU,EAAE,OAAQ,GAArB",\n\t"names": []\n}'),
-          map: expect.it('when decoded as', 'utf8', function (str) {
-            var obj = JSON.parse(str);
-
-            expect(obj, 'to satisfy', {
-              version: 3,
-              file: 'foo.css',
-              sources: ['stdin'],
-              sourcesContent: [],
-              mappings: expect.it('to be a string'),
-              names: []
-            });
+          map: expect.it('when decoded as', 'utf8', JSON.parse, 'to satisfy', {
+            version: 3,
+            file: 'foo.css',
+            sources: ['stdin'],
+            sourcesContent: [],
+            mappings: expect.it('to be a string'),
+            names: []
           }),
           stats: expect.it('to satisfy', {
             includedFiles: []
